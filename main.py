9ddd08295f2f8ea7c3540cae52081f47ccb95eb3
sami issa <samiissa4011@gmail.com>
2026-03-15 08:44:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import sqlite3
 
 app = FastAPI()
 
-# الاتصال بقاعدة البيانات
 conn = sqlite3.connect("hospital.db", check_same_thread=False)
 cursor = conn.cursor()
 
-# إنشاء جدول المرضى إذا لم يكن موجود
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS patients (
     id INTEGER PRIMARY KEY,
@@ -19,18 +17,32 @@ CREATE TABLE IF NOT EXISTS patients (
 
 conn.commit()
 
-
 class Patient(BaseModel):
     id: int
     name: str
     condition: str
 
-
 @app.get("/")
 def home():
     return {"message": "Hospital System Running"}
 
-
 @app.get("/patients")
 def get_patients():
     cursor.execute("SELECT * FROM patients")
+    rows = cursor.fetchall()
+    return rows
+
+@app.post("/patients")
+def add_patient(patient: Patient):
+    cursor.execute(
+        "INSERT INTO patients (id,name,condition) VALUES (?,?,?)",
+        (patient.id, patient.name, patient.condition)
+    )
+    conn.commit()
+    return {"message": "Patient added"}
+
+@app.delete("/patients/{patient_id}")
+def delete_patient(patient_id: int):
+    cursor.execute("DELETE FROM patients WHERE id=?", (patient_id,))
+    conn.commit()
+    return {"message": "Patient deleted"}
